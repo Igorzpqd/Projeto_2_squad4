8d92a5b07cdd78d1f2ebdaa79410bac077094f37
Daniella Werneck <daniella.silqueiroz@gmail.com>
2023-01-09 04:36:33 -0300

    Ultimas edicoes

--- js/ValidacoesJS.js
+++ js/ValidacoesJS.js
@@ -1,4 +1,5 @@
 class ValidacoesJS {
+  
 
 
     // constructor() {
@@ -7,6 +8,7 @@ class ValidacoesJS {
     
     buscarRG = () => {
       document.getElementById("buscar").addEventListener("click", function(event){
+        let pcep = document.getElementById('CEP');
       console.log('teste')
 
         //Nova variável "cep" somente com dígitos.
@@ -26,19 +28,22 @@ class ValidacoesJS {
              
                     if (!("erro" in dados)) {
                         //Atualiza os campos com os valores da consulta.
-                        $("#cadastroRua").val(dados.logradouro+","+dados.complemento);
+                        $("#cadastroRua").val(dados.logradouro);
                         $("#cadastroBairro").val(dados.bairro);
                         $("#cadastroCidade").val(dados.localidade);
                         $("#cadastroEstado").val(dados.uf);
+                        $("#cadastroComplemento").val(dados.complemento);
                     } //end if.
                     else {
                         //CEP pesquisado não foi encontrado.
-                        console.log("CEP não encontrado.");
+                        pcep.innerHTML = "CEP não encontrado";
+                        pcep.style.color = "red";
                     }
                 });
             } //end if.
             else {
-                console.log("Formato de CEP inválido.");
+              pcep.innerHTML = "CEP Inválido";
+              pcep.style.color = "red";
             }
             
         } //end if.
@@ -60,6 +65,18 @@ class ValidacoesJS {
             let psenha2 = document.getElementById('Senha2');
             let rg = document.getElementById('cadastroRG').value;
             let pRG = document.getElementById('RG');
+            let cep = document.getElementById('cadastroCEP').value;
+            let pcep = document.getElementById('CEP');
+            let estado = document.getElementById('cadastroEstado').value;
+            let pEstado = document.getElementById('Estado');
+            let cidade = document.getElementById('cadastroCidade').value;
+            let pCidade = document.getElementById('Cidade');
+            let bairro = document.getElementById('cadastroBairro').value;
+            let pBairro = document.getElementById('Bairro');
+            let rua = document.getElementById('cadastroRua').value;
+            let pRua = document.getElementById('Numero');
+            let complemento = document.getElementById('cadastroComplemento').value;
+            let pcomplemento = document.getElementById('Complemento');
             
            console.log('nome:',nome)
            console.log('email:',email)
@@ -117,6 +134,37 @@ class ValidacoesJS {
                 pRG.innerText = " * RG inválido ";
                 pRG.style.color  = "red";
                }
+               //cep
+               if (cep == "") {
+                pcep.innerText = " * Campo CEP Vazio, preencha correctamente";
+                pcep.style.color  = "red";
+               
+               } 
+               if (estado == "") {
+                pEstado.innerText = " * Campo ESTADO Vazio, preencha correctamente";
+                pEstado.style.color  = "red";
+               
+               } 
+               if (cidade == "") {
+                pCidade.innerText = " * Campo CIDADE Vazio, preencha correctamente";
+                pCidade.style.color  = "red";
+               
+               } 
+               if (bairro == "") {
+                pBairro.innerText = " * Campo BAIRRO Vazio, preencha correctamente";
+                pBairro.style.color  = "red";
+               
+               }
+               if (rua == "") {
+                pRua.innerText = " * Campo RG RUA, preencha correctamente";
+                pRua.style.color  = "red";
+               
+               }
+               if (complemento == "") {
+                pcomplemento.innerText = " * Campo COMPLEMENTO Vazio, preencha correctamente";
+                pcomplemento.style.color  = "red";
+               
+               }
 
             
 
@@ -128,13 +176,75 @@ class ValidacoesJS {
 
        
     }
-
-    login = () => {
+    envioEmail = () =>{
+      
+      document.getElementById("enviar").addEventListener("click", function(event){
+        let email = document.getElementById("emailRec").value
+        let pemail = document.getElementById("pemail")
+        if(email == ""){
+          pemail.innerText = " * Campo Email Vazio";
+          pemail.style.color  = "red";
+        }else{
+          Email.send({
+            Host: "smtp.gmail.com",
+            Username: `${email}`,
+            Password: "Enter your password",
+            To: email,
+            From: "daniella.silqueiroz@gmail.com",
+            Subject: "Sending Email using javascript",
+            Body: "Well that was easy!!",
+            Attachments: [
+              {
+                name: "File_Name_with_Extension",
+                path: "Full Path of the file"
+              }]
+            })
+            .then(function () {
+              window.location.href = "mensagemEmail.html";
+            });
+          }
+            event.preventDefault()
+          });
 
     }
 
-    esqueceuSenha = () => {
-        
+    login = () => {
+      document.getElementById("entrar").addEventListener("click", function(event){
+        let emailTeste = "teste@gmail.com";
+        let senhaTeste = "123456";
+        let email = document.getElementById("email").value;
+        let senha = document.getElementById("senha").value;
+        let pemail = document.getElementById("pemail");
+        let psenha = document.getElementById("psenha");
+
+        //email
+       if(email == ""){
+          pemail.innerHTML ="Email  Vazio"
+          pemail.style.color  = "red";
+        }else if(email != emailTeste ){
+          pemail.innerHTML ="Usuário inválido"
+          pemail.style.color  = "red";
+        }
+        else{
+          pemail.innerHTML =""
+        }
+        //senha
+        if(senha == "" ){
+          psenha.innerHTML ="Senha Vazia"
+          psenha.style.color  = "red";
+        }else if(senha !== senhaTeste ){
+          psenha.innerHTML ="Senha inválida"
+          psenha.style.color  = "red";
+        }
+        else{
+          psenha.innerHTML =""
+        }
+        if(email == emailTeste &&senha == senhaTeste){
+          window.location.href = "mensagemEmail.html";
+
+        }
+        event.preventDefault()
+      });
     }
 
 
